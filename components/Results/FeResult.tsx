8a2d0018ca9aed256bdb5dca6f2d1c029a7b4d93
MatixYo <matixovi@gmail.com>
2021-12-12 07:56:00 +0100

    Add tooltip

--- components/Results/FeResult.tsx
+++ components/Results/FeResult.tsx
@@ -1,13 +1,19 @@
 import s from "./Result.module.css";
-import React from "react";
+import React, { useMemo } from "react";
+import ReactTooltip from "react-tooltip";
 
 const Result: React.FC<{ item: any }> = ({ item }) => {
+  const id = useMemo(() => Math.random() * 100000000, []);
+
   return (
-    <li className={s.feResult}>
+    <li className={s.feResult} data-tip data-for={`item-${id}`}>
       <a href={item.link} target="_blank" rel="noreferrer">
         <div className={s.title}>{item.name}</div>
         <div dangerouslySetInnerHTML={{ __html: item.content }} />
       </a>
+      <ReactTooltip id={`item-${id}`} effect="solid" className="max-w-lg">
+        <div dangerouslySetInnerHTML={{ __html: item.content }} />
+      </ReactTooltip>
     </li>
   );
 };
